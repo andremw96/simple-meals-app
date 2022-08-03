a30f1a1d5e59ca8745aca56581587aeb296aa541
andremw96 <andre19961@gmail.com>
2022-08-03 15:11:42 +0700

    have category screen

---
+++ lib/categories_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/category_item.dart';
+import 'package:meals_app/models/category.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "dreMeal",
+        ),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES.map((e) {
+          return CategoryItem(
+            title: e.title,
+            color: e.color,
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
